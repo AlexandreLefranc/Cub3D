bcd6634bdad81414c4e77cbd67755a9e52d1e1d8
louisea <louiseamelie2@gmail.com>
2022-08-25 15:32:20 +0200

    map_parser avec check des contours done

--- src/map_parser.c
+++ src/map_parser.c
@@ -6,13 +6,23 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 11:54:29 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/08/25 12:36:12 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/08/25 15:31:17 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #include "cub3d.h"
+
+static int	char_is_valid(char c, int line, int column)
+{
+	if (c == '1' || c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W'
+	|| c == 32)
+	return (1);
+	else
+	{
+		printf("char %c is invalid at [%d][%d]\n", c, line, column);
+		return (0);
+	}
+}
 
 static int	spaces_are_valid(int line, int column, char **map)
 {
@@ -22,17 +32,92 @@ static int	spaces_are_valid(int line, int column, char **map)
 			&& map[line - 1][column] != 32 && map[line + 1][column] != 32)
 			return (1);
 		else
+		{
+			printf("spaces are invalid\n");
 			return (0);
+		}
+	}
+	return (1);
+}
+//
+// static int	last_column_check(char *map)
+// {
+// 	int	last_column;
+//
+// 	last_column = (int)ft_strlen(map) - 1;
+// 	if (map[last_column] != '1' && map[last_column] != 32) // check que dernier char soit un espace ou un mur
+// 		return (0);
+// 	if (map[last_column] == '1') // si dernier char est un mur return true
+// 		return (1);
+// 	while (map[last_column] == 32)
+// 		last_column--; // si dernier char = espace -> recule jusqu'au dernier espace
+// 	if (map[last_column] != '1') // verifie que 1er char apres dernier expace soit un mur
+// 		return (0);
+// 	return (1);
+// }
+
+static int	column_borders_check(char *map)
+{
+	int	first_column;
+	int	last_column;
+
+	last_column = (int)ft_strlen(map) - 1;
+	first_column = 0;
+	if ((map[first_column] != '1' && map[first_column] != 32)
+		|| (map[last_column] != '1' && map[last_column] != 32)) // check que premier char soit un espace ou un mur
+	{
+		printf("wrong column border first_column : %d = %c\n", first_column, map[first_column]);
+		printf("wrong column border last_column : %d = %c\n", last_column, map[last_column]);
+		return (0);
+	}
+	if (map[first_column] == '1' && map[last_column] == '1') // si prmier char est un mur return true
+		return (1);
+	while (map[first_column] == 32)
+		first_column++; // avance jusqu'au dernier espace
+	if (map[first_column] != '1') // verifie que le premier char apres le dernier espace soit un mur
+	{
+		printf("first column with spaces issue -> map[%d] = %c\n", first_column, map[first_column]);
+		return (0);
+	}
+	while (map[last_column] == 32)
+		last_column--; // si dernier char = espace -> recule jusqu'au dernier espace
+	if (map[last_column] != '1') // verifie que 1er char apres dernier expace soit un mur
+	{
+		printf("last column with spaces issue -> map[%d] = %c\n", last_column, map[last_column]);
+		return (0);
 	}
 	return (1);
 }
 
-static int	char_is_valid(char c)
+static int	line_borders_check(char **map)
 {
-	if (c == '1' || c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W'
-		|| c == 32)
-		return (1);
-	return (0);
+	int	last_line;
+	int	column;
+
+	column = 0;
+	last_line = ft_strtabsize(map) - 1;
+	while (map[0][column])
+	{
+		if (map[0][column] == '1' || map[0][column] == 32) // check premiere ligne
+			column++;
+		else
+		{
+			printf("first line ko: map[0][%d] = %d\n", column, map[0][column]);
+			return (0);
+		}
+	}
+	column = 0;
+	while (map[last_line][column])
+	{
+		if (map[last_line][column] == '1' || map[last_line][column] == 32)// check derniere ligne
+			column++;
+		else
+		{
+			printf("last line ko: map[0][%d] = %d\n", column, map[last_line][column]);
+			return (0);
+		}
+	}
+	return (1);
 }
 
 int	map_is_valid(char **map)
@@ -41,15 +126,21 @@ int	map_is_valid(char **map)
 	int	column;
 
 	line = 0;
-	while (map[line])
+	if (line_borders_check(map) == 0)
+		return (0);
+	while (map[line] && line < (int)ft_strtabsize(map))
 	{
 		column = 0;
-		while (map[line][column])
+		if (column_borders_check(map[line]) == 0)
+			// || last_column_check(map[line]) == 0)
+			return (0);
+		while (map[line][column] && column < (int)ft_strlen(map[line]))
 		{
-			if (char_is_valid(map[line][column]) == 1
+			if (char_is_valid(map[line][column], line, column) == 1
 				&& spaces_are_valid(line, column, map) == 1)
 				column++;
-			return (0);
+			else
+				return (0);
 		}
 		line++;
 	}
